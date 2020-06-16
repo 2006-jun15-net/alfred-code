8b3eb3819fd3fe0fac8de6fd03f70ca4738a35df
Alfred <arwagaju@gmail.com>
2020-06-16 18:22:23 -0400

    The user can input the number of stairs of their choice

---
+++ modifiedStairCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace modifiedStairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            modifiedStaircase();
+        
+        }
+
+         static void modifiedStaircase()
+        {
+            Console.WriteLine("Enter the size of the staircase");
+            String number = Console.ReadLine();
+            int size = Convert.ToInt32(number);
+             for(int i = 0; i <= size; i++)
+            {
+                for(int j = 0; j <= size; j++)
+                {
+                    if((i+j) > size)
+                    {
+                        Console.Write("#");
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }  
+    }
+}
