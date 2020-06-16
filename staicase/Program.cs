8157f8dcc0145de36828d1998c9e6bd1c7179f04
Alfred <arwagaju@gmail.com>
2020-06-16 18:02:45 -0400

    implemented a staircase of size 8

---
+++ staicase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace staicase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            staircase();
+           
+            
+        }
+
+        static void staircase()
+        {
+             for(int i = 0; i <= 8; i++)
+            {
+                for(int j = 0; j <= 8; j++)
+                {
+                    if((i+j) > 8)
+                    {
+                        Console.Write("#");
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }  
+    }
+
+   
+}
