3145d9380a6c60b661cdcd01c8c06b23f82a4e3e
Alfred <arwagaju@gmail.com>
2020-06-16 18:59:00 -0400

    Implemented a program that computes a Collatz sequence

---
+++ collatz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace collatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            String n = Console.ReadLine();
+            int number = Convert.ToInt32(n);
+            int num = number;
+            while(num != 1)
+            {
+                if(num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num = (num*3) + 1;
+
+                }Console.WriteLine(num);
+            }
+
+           
+        }
+    }
+}
