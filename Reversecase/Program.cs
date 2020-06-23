859542360e8bf2b572171c68d6d182bf4cab1235
Alfred <arwagaju@gmail.com>
2020-06-22 23:30:33 -0400

    implemented a method that reverses a string and changes the case

---
+++ Reversecase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Reversecase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "abcDeFgHi";
+
+            Console.WriteLine(ReverseCase(str));
+            
+        }
+
+        static string ReverseCase(string str)
+        {
+            //creating a stringBuilder object
+            //StringBuilder strbuilder = new StringBuilder(str);
+            string reversedString = " ";
+            for(int i = str.Length -1; i >= 0; i--)
+            {
+                if(Char.IsUpper(str[i]))
+                {
+              
+                    reversedString += Char.ToLower(str[i]);
+                }
+                else if(Char.IsLower(str[i]))
+                {
+                  
+                    reversedString += Char.ToUpper(str[i]);
+                }
+               
+            }
+            return reversedString;
+        }
+    }
+}
