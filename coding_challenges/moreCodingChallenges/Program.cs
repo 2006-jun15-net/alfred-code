6636498a8fab9353db8c4a3489b8f61a1c322145
alfredrwagaju <arwagaju@gmail.com>
2020-09-05 22:50:53 -0400

    implemented maximumGap

---
+++ coding_challenges/moreCodingChallenges/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace moreCodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {3,6,9,1,35};
+            System.Console.WriteLine(maximumGap(array));
+            
+            
+        }
+
+        static int maximumGap(int[] array)
+        {
+            //sort the array first using selection sort
+            int temp = 0;
+            int maxGap =0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                int max = i;
+                for(int j = i+1; j < array.Length; j++)
+                {
+                    if(array[max] > array[j])
+                    {
+                        temp = array[max];
+                        array[max] = array[j];
+                        array[j] = temp;
+                        
+                    }
+                }
+            }
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = i+1; j < array.Length; j++)
+                {
+                    if(j-i == 1)
+                    {
+                        int gap = array[j] - array[i];
+                        if(gap > maxGap)
+                        {
+                            maxGap = gap;
+                        }
+                    }
+                }
+            }return maxGap;
+        }
+        
+    }
+}
