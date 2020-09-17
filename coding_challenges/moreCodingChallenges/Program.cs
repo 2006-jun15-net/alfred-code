13dda9b78e370b0d910004025d49f0a3da8df578
alfredrwagaju <arwagaju@gmail.com>
2020-09-17 01:30:56 -0400

    did more coding challenges in the Program.cs file

--- coding_challenges/moreCodingChallenges/Program.cs
+++ coding_challenges/moreCodingChallenges/Program.cs
@@ -5,6 +5,7 @@ using AnagramCheck;
 using OccurenceArray;
 using PairSumProgram;
 using RemoveDuplicates;
+using System.Collections.Generic;
 
 namespace moreCodingChallenges
 {
@@ -32,22 +33,95 @@ namespace moreCodingChallenges
             // int[] array = {1,3,2,2};
             // System.Console.WriteLine(findPair.CountPairSum(array,4));
 
-            StringDuplicate stringDuplicate = new StringDuplicate();
-            string str = "looooove";
-            System.Console.WriteLine(stringDuplicate.RemoveDuplicateString(str));
+            // StringDuplicate stringDuplicate = new StringDuplicate();
+            // string str = "looooove";
+            // System.Console.WriteLine(stringDuplicate.RemoveDuplicateString(str));
+
+            //int[] array = {1,2,3,4,5,6,7,8,9};
+            //System.Console.WriteLine(findMissingNumber(array,1,10));
+
+            //string s = "love";
+            
+            //System.Console.WriteLine(CheckUnique(s));
+
+            int num = 34431;
+            System.Console.WriteLine(checkPalindromic(num));
+
+
 
             
-            
+        }
+        /// <summary>
+        /// returns the missing number in an array of a starting to ending integer.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
 
-            
+        public static int findMissingNumber(int[] array, int start, int end)
+        {
+            int missingNumber = 0;
+            foreach(int num in array)
+            {
+                for(int i = start; i <= end; i++)
+                {
+                    if(i != num)
+                    {
+                        missingNumber = i;
+                    }
 
-            
+                }
+            }return missingNumber;
+        }
 
+        /// <summary>
+        /// checks if a string has unique characters.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+
+        public static bool CheckUnique(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
             
+            foreach(char chr in str)
+            {
+                if(dict.ContainsKey(chr))
+                {
+                    return false;     
+                    
+                }
+                else
+                {
+                    dict.Add(chr,1);
+                } 
+
+            }
+            return true;
+
+        }
 
 
+        /// <summary>
+        /// checks if a number is palindromic.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool checkPalindromic(int num)
+        {
+            string StringNum = num.ToString();
+            string reversedString = "";
+            for(int i = 0; i < StringNum.Length; i++)
+            {
+                reversedString += StringNum[StringNum.Length-1-i];
+            }
+            if(reversedString.Equals(StringNum))
+            {
+                return true;
+            }
             
-            
+            return false;
         }
 
         
